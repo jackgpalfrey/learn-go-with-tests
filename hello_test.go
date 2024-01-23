3aed94fdc53792fceee9dbdabb40c07addb33745
Jack Palfrey <jackgpalfrey@outlook.com>
2024-01-23 14:56:18 +0000

    refactor: hello_test

--- hello_test.go
+++ hello_test.go
@@ -7,18 +7,21 @@ func TestHello(t *testing.T) {
 		name := "Jack"
 		got := Hello(name)
 		want := "Hello, " + name
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to world", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
 	})
+}
+
+
+func assertCorrectMessage(t testing.TB, got string, want string){
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
